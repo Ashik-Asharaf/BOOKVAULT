a7ea6cb2fb9d010448b151df3af5752e4a976af4
Ashik-Asharaf <ashikasharaf2120@gmail.com>
2025-07-16 21:25:37 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+ const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+//create a db fn
+
+mongoose.connect(
+    "mongodb+srv://ashikasharaf2120:Ashik%402120@cluster0.4opg4ml.mongodb.net/"
+).then(() => console.log("Connected to MongoDB")).catch((error) => console.log(error));
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: "http://localhost:5173/",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", 
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma",
+        ],
+        credentials: true,
+    })
+)
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
