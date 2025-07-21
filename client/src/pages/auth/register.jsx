1899e39b584b4896ca212343a35ca99b34d314d2
Ashik-Asharaf <ashikasharaf2120@gmail.com>
2025-07-21 21:19:12 +0530

    edit

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,7 +1,9 @@
 import CommonForm from "@/components/common/form";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import { registerFormControls } from "@/config";
+import { useDispatch } from "react-redux";
+//import { useNavigate } from "react-router-dom";
 
 const initialState = {
     username: '',
